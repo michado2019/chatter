658e16bbe99cd77fe55d102791b98f73711ac5a9
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-06-14 20:55:24 +0100

    feat & style: ChatterWelcome.tsx done and styled

--- src/components/pages/landingPage/chatterWelcome/ChatterWelcome.tsx
+++ src/components/pages/landingPage/chatterWelcome/ChatterWelcome.tsx
@@ -1,15 +1,22 @@
-import "./ChatterWelcome.css"
+import { Link } from "react-router-dom";
+import "./ChatterWelcome.css";
 const ChatterWelcome = () => {
   return (
-    <div className="chatterWelcome-wrapper">ththhth hhlhh hlkhlhl hdkfhdk gdkhfgdk kfgkdgk kgdgkdg kdgfdkg
-    kdgkgdkdgfkg kdgkdgk gkdgkdgk gkgkgdfkgfk gkdgfkdgfk gdkgfdkg kfgdkgfk
-    gkfgdk gkdgfkdgfkg kdgkfgkg fkdg kgkdgfkg dkgfkgkg dkgkdgfkdg kdgfkdgkdg
-    kgfdkgfkd gdkgfdkgfdkgfkdgk gkdgkgd ththhth hhlhh hlkhlhl hdkfhdk
-    gdkhfgdk kfgkdgk kgdgkdg kdgfdkg kdgkgdkdgfkg kdgkdgk gkdgkdgk
-    gkgkgdfkgfk gkdgfkdgfk gdkgfdkg kfgdkgfk gkfgdk gkdgfkdgfkg kdgkfgkg
-    fkdg kgkdgfkg dkgfkgkg dkgkdgfkdg kdgfkdgkdg kgfdkgfkd gdkgfdkgfdkgfkdgk
-    gkdgkgd</div>
-  )
-}
+    <div className="chatterWelcome-wrapper">
+      <div className="chatterWelcome-contents">
+        <h2 className="chatterWelcome-intro">
+          Welcome to Chatter: A Haven for Text-Based Content
+        </h2>
+        <p className="chatterWelcome-details">
+          Welcome to Chatter: A Haven for Text-Based Content Unleash the Power
+          of Words, Connect with Like-minded Readers and Writers Get started
+        </p>
+        <Link to="/sign-up" className="chatterWelcome-btn">
+          Get started
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default ChatterWelcome
+export default ChatterWelcome;
