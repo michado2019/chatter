a1e5e054b452d4fabd049a18f346ab77164df844
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-06-21 21:16:08 +0100

    style: Login.css modified

--- src/components/pages/register/Register.tsx
+++ src/components/pages/register/Register.tsx
@@ -1,20 +1,28 @@
 import "./Register.css";
-import { useState } from "react";
-import {
-  Visibility,
-  VisibilityOff,
-} from "@mui/icons-material";
+import React, { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import googleImg from "./assets/googleImg.png";
 import linkedInImg from "./assets/linkedInImg.png";
-import comfirmPImg from "./assets/VectorconfirmP.png"
+import comfirmPImg from "./assets/VectorconfirmP.png";
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  userType: string;
+}
+
 const Register = () => {
-  //State
-  const [formData, setFormData] = useState({
+  // State
+  const [formData, setFormData] = useState<FormData>({
     firstName: "",
     lastName: "",
     email: "",
     password: "",
-    comfirmPssord: "",
+    confirmPassword: "",
+    userType: "Writer",
   });
 
   const [visibility, setVisibility] = useState(false);
@@ -23,11 +31,14 @@ const Register = () => {
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
-    // Form submission for post
+    // Form submission logic
+    console.log(formData);
   };
 
   // Input change handler
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = event.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
@@ -48,6 +59,10 @@ const Register = () => {
                 placeholder="John"
                 className="registerForm1-input"
                 id="registerForm1-input1"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleInputChange}
+                required
               />
             </div>
             <div className="registerForm1-flex2">
@@ -57,15 +72,24 @@ const Register = () => {
                 placeholder="Doe"
                 className="registerForm1-input"
                 id="registerForm1-input2"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleInputChange}
+                required
               />
             </div>
           </div>
         </div>
         <div className="registerForm1">
           <label className="registerLabel">Are you joining as?</label>
-          <select className="registerForm1-input2">
-            <option>Writer</option>
-            <option>Reader</option>
+          <select
+            className="registerForm1-input2"
+            name="userType"
+            value={formData.userType}
+            onChange={handleInputChange}
+          >
+            <option value="Writer">Writer</option>
+            <option value="Reader">Reader</option>
           </select>
         </div>
         <div className="registerForm1-flexx">
@@ -76,15 +100,23 @@ const Register = () => {
               placeholder="johndoe@gmail.com"
               className="registerForm2"
               id="registerForm2"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
             />
           </div>
           <div className="registerForm1-flex2">
             <label className="registerLabel">Password</label>
             <div className="registerForm-flex_row">
               <input
-                type={visibility ? "text/password" : "password"}
+                type={visibility ? "text" : "password"}
                 placeholder="*********"
                 className="registerForm2"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                required
               />
               <Visibility
                 className="visibility"
@@ -102,31 +134,36 @@ const Register = () => {
             <label className="registerLabel">Confirm password</label>
             <div className="registerForm-flex_row">
               <input
-                type={visibility ? "text/password" : "password"}
+                type={visibility ? "text" : "password"}
                 placeholder="*********"
                 className="registerForm2"
                 id="registerForm22"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
+                required
               />
-              <img src={comfirmPImg} alt="img"
+              <img
+                src={comfirmPImg}
+                alt="img"
                 className="visibility"
                 onClick={() => setVisibility((prev) => !prev)}
               />
             </div>
           </div>
         </div>
-        <button className="signUp-btn" id="signUp-btn">
+        <button className="signUp-btn" id="signUp-btn" type="submit">
           Create account
         </button>
       </form>
       <div className="registerForm1" id="registerForm1">
         <button className="signUp-btn" id="signUp-btn_white">
           <img src={googleImg} alt="img" className="signUp-btn_icon" />
-          Sign up with google
+          Sign up with Google
         </button>
         <button className="signUp-btn" id="signUp-btn_white">
-          {" "}
           <img src={linkedInImg} alt="img" className="signUp-btn_icon" /> Sign
-          up with linkedin
+          up with LinkedIn
         </button>
       </div>
     </div>
