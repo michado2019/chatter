b018808717bf231191d9d3959509c2d5c1612362
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-06-29 22:41:41 +0100

    BlogSidebar.tsx and its style sheet added

---
+++ src/components/pages/blogSidebar/BlogSidebar.tsx
@@ -0,0 +1,12 @@
+import "./BlogSidebar.css"
+const BlogSidebar = () => {
+  return (
+    <div className="blogSidebar-wrapper">
+        <div className="blogSidebar-contents">
+        
+        </div>
+    </div>
+  )
+}
+
+export default BlogSidebar
