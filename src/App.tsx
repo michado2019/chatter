a41fbfcb7e45641447e44d104d00196e52bba1a7
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-07-18 02:08:53 +0100

    feat: error boundary added

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import { navbarLinks } from "./components/layouts/navbar/navbarData/NavabarData"
 import SmallScreenNav from "./components/pages/smallScreenNav/SmallScreenNav";
 import AppRouter from "./components/route";
 import { UserContext } from "./components/context/userContext/UserContext";
+import ErrorBoundary from "./components/pages/errorBoundary/ErrorBoundary";
 
 function App() {
   const userContext = useContext(UserContext);
 
   return (
     <div className="App">
-      <SmallScreenNav navbarLinks={navbarLinks} />
-      <AppRouter />
-      {userContext?.user === null && <Footer />}
+      <ErrorBoundary>
+        <SmallScreenNav navbarLinks={navbarLinks} />
+        <AppRouter />
+        {userContext?.user === null && <Footer />}
+      </ErrorBoundary>
     </div>
   );
 }
+
 export default App;
---
+++ src/components/pages/errorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import React, { Component, ErrorInfo, ReactNode } from "react";
+import "./ErrorBoundary.css";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false
+    };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    console.error("ErrorBoundary caught an error:", error, info);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-boundary">
+          <h2>Oops! Something went wrong.</h2>
+          <p>We apologize for the inconvenience. Please try again later.</p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
