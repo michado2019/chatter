ec2c0b94011ba2398f6bf2adbf29e1ee2a3c371e
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-08-02 10:41:33 +0100

    Footer.tsx rendered in Home.tsx even if no user exists modified

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,13 +1,15 @@
 import "./Home.css";
 import LandingPage from "../landingPage/LandingPage";
 import Footer from "../../layouts/footer/Footer";
-
+import { UserContext } from "../../context/userContext/UserContext";
+import { useContext } from "react";
 const Home = () => {
+  const userContext = useContext(UserContext);
   return (
     <div className="homeWrapper">
       <div className="homeContents">
         <LandingPage />
-        <Footer />
+        {userContext?.user === null ? "" : <Footer />}
       </div>
     </div>
   );
