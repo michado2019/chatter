f6b4818881262bd6c8dfac83db43406d270f7c04
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-08-02 10:33:50 +0100

    Footer.tsx rendered in Home.tsx even if no user exists

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import "./Home.css";
 import LandingPage from "../landingPage/LandingPage";
+import Footer from "../../layouts/footer/Footer";
 
 const Home = () => {
   return (
     <div className="homeWrapper">
       <div className="homeContents">
         <LandingPage />
+        <Footer />
       </div>
     </div>
   );
